9baa521ad98dc531df6ed17e4b1e188b0292e9df
Perekatee <ivan.vedyaykin@yandex.ru>
2024-01-16 13:36:48 +0300

    SecondOne

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 500;
-        int y = 1500;
+        int account = 500;
+        int added = 1500;
         int bonus;
-        int sum = x+y;
-        if (y < 1000) {
+        int sum = account + added;
+        if (added < 1000) {
             bonus = 0;
         } else {
-            bonus = y / 100;
+            bonus = added / 100;
         }
 
         System.out.println(bonus);
