8dd41bf33df5ac0e9f81055056393ddcb034fb46
Perekatee <ivan.vedyaykin@yandex.ru>
2024-01-17 18:43:02 +0300

    thirdtry

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int account = 500;
-        int added = 1500;
+        int account = 100;
+        int added = 1100;
         int bonus;
         int sum = account + added;
         if (added < 1000) {
             bonus = 0;
         } else {
             bonus = added / 100;
+            sum = account + added + bonus;
         }
 
         System.out.println(bonus);
